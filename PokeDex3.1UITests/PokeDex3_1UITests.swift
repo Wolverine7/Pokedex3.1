75e0bd5188e0a901481b30e8e420715e7e878e87
Wolverine7 <bisamov@gmail.com>
2017-10-28 14:09:18 -0500

    Initial Commit

---
+++ PokeDex3.1UITests/PokeDex3_1UITests.swift
@@ -0,0 +1,36 @@
+//
+//  PokeDex3_1UITests.swift
+//  PokeDex3.1UITests
+//
+//  Created by Bobodzhon Isamov on 10/28/17.
+//  Copyright © 2017 Bobodzhon Isamov. All rights reserved.
+//
+
+import XCTest
+
+class PokeDex3_1UITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
